711a99659f08b2e13af2389e1dc2d9f6b6e08e8c
lebrancconvas <lebrancconvas@gmail.com>
2024-08-24 09:27:34 +0700

    ✅ test: passed component 1-bit multiplexer (mux) test

--- digital_curcuit/src/tests/component.test.ts
+++ digital_curcuit/src/tests/component.test.ts
@@ -16,3 +16,37 @@ describe("Test Component: Switcher", () => {
     expect(Component.switcher(1, 1)).toBe(1);
   })
 });
+
+describe("Test Component: 1-Bit Multiplexer (Mux)", () => {
+  test("Test 1-Bit Mux - Selector: 0, Input1: 0, Input2: 0 must return 0", () => {
+    expect(Component.mux1(0, 0, 0)).toBe(0);
+  })
+
+  test("Test 1-Bit Mux - Selector: 0, Input1: 0, Input2: 1 must return 1", () => {
+    expect(Component.mux1(0, 0, 1)).toBe(0);
+  })
+
+  test("Test 1-Bit Mux - Selector: 0, Input1: 1, Input2: 0 must return 1", () => {
+    expect(Component.mux1(0, 1, 0)).toBe(1);
+  })
+
+  test("Test 1-Bit Mux - Selector: 0, Input1: 1, Input2: 1 must return 1", () => {
+    expect(Component.mux1(0, 1, 1)).toBe(1);
+  })
+
+  test("Test 1-Bit Mux - Selector: 1, Input1: 0, Input2: 0 must return 0", () => {
+    expect(Component.mux1(1, 0, 0)).toBe(0);
+  })
+
+  test("Test 1-Bit Mux - Selector: 1, Input1: 0, Input2: 1 must return 1", () => {
+    expect(Component.mux1(1, 0, 1)).toBe(1);
+  })
+
+  test("Test 1-Bit Mux - Selector: 1, Input1: 1, Input2: 0 must return 0", () => {
+    expect(Component.mux1(1, 1, 0)).toBe(0);
+  })
+
+  test("Test 1-Bit Mux - Selector: 1, Input1: 1, Input2: 1 must return 1", () => {
+    expect(Component.mux1(1, 1, 1)).toBe(1);
+  })
+});
