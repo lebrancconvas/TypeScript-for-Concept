2d0a14cab97ae9a3588b0cbbf358332afe1d9328
lebrancconvas <lebrancconvas@gmail.com>
2024-08-24 14:48:54 +0700

    🧪 test: update failed comparator1 arithmatic component test

--- digital_curcuit/src/tests/arithmatic.test.ts
+++ digital_curcuit/src/tests/arithmatic.test.ts
@@ -19,3 +19,21 @@ describe("Test Arithmatic: Half-Adder", () => {
     expect(Arithmatic.halfAdder(1, 1)).toEqual([0, 1]);
   })
 });
+
+describe("Test Arithmatic: 1-Bit Comparator (comparator1)", () => {
+  test("Test Comparator1 - Input1: 0, Input2: 0 must return lessThan: 0, equal: 1, greaterThan: 0", () => {
+    expect(Arithmatic.comparator1(0, 0)).toEqual([0, 1, 0]);
+  })
+
+  test("Test Comparator1 - Input1: 0, Input2: 1 must return lessThan: 1, equal: 0, greaterThan: 0", () => {
+    expect(Arithmatic.comparator1(0, 1)).toEqual([1, 0, 0]);
+  })
+
+  test("Test Comparator1 - Input1: 1, Input2: 0 must return lessThan: 0, equal: 0, greaterThan: 1", () => {
+    expect(Arithmatic.comparator1(1, 0)).toEqual([0, 0, 1]);
+  })
+
+  test("Test Comparator1 - Input1: 1, Input2: 1, Input3: 1 must return lessThan: 0, equal: 1, greaterThan: 0", () => {
+    expect(Arithmatic.comparator1(1, 1)).toEqual([0, 1, 0]);
+  })
+});
