7b9d647d4d478f7ce91fce83878665341283595b
lebrancconvas <lebrancconvas@gmail.com>
2024-08-24 09:03:33 +0700

    ✅ test: passed logic gate tests (not, and, or)

---
+++ digital_curcuit/src/tests/gate.test.ts
@@ -0,0 +1,50 @@
+import { Gate } from "../libs";
+import { describe, test, expect } from "@jest/globals";
+
+
+describe("Test Logic NOT Gate", () => {
+  test("Test NOT Gate - Input: 0 must return 1", () => {
+    expect(Gate.not(0)).toBe(1);
+  })
+
+  test("Test NOT Gate - Input: 1 must return 0", () => {
+    expect(Gate.not(1)).toBe(0);
+  })
+});
+
+describe("Test Logic AND Gate", () => {
+  test("Test AND Gate - Input: 0, 0 must return 0", () => {
+    expect(Gate.and(0, 0)).toBe(0);
+  });
+
+  test("Test AND Gate - Input: 0, 1 must return 0", () => {
+    expect(Gate.and(0, 1)).toBe(0);
+  });
+
+  test("Test AND Gate - Input: 1, 0 must return 0", () => {
+    expect(Gate.and(1, 0)).toBe(0);
+  });
+
+  test("Test AND Gate - Input: 1, 1 must return 1", () => {
+    expect(Gate.and(1, 1)).toBe(1);
+  });
+});
+
+describe("Test Logic OR Gate", () => {
+  test("Test OR Gate - Input: 0, 0 must return 0", () => {
+    expect(Gate.or(0, 0)).toBe(0);
+  });
+
+  test("Test OR Gate - Input: 0, 1 must return 1", () => {
+    expect(Gate.or(0, 1)).toBe(1);
+  });
+
+  test("Test OR Gate - Input: 1, 0 must return 1", () => {
+    expect(Gate.or(1, 0)).toBe(1);
+  });
+
+  test("Test OR Gate - Input: 1, 1 must return 1", () => {
+    expect(Gate.or(1, 1)).toBe(1);
+  });
+})
+
