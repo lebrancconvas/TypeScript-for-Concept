eac49bf046d4f52ed0338616789e6cc46b350cca
lebrancconvas <lebrancconvas@gmail.com>
2024-08-24 09:37:57 +0700

    ✅ test: passed component demultiplexer (demux) test

--- digital_curcuit/src/tests/component.test.ts
+++ digital_curcuit/src/tests/component.test.ts
@@ -50,3 +50,21 @@ describe("Test Component: 1-Bit Multiplexer (Mux)", () => {
     expect(Component.mux1(1, 1, 1)).toBe(1);
   })
 });
+
+describe("Test Component: 1-Bit Demultiplexer (Demux", () => {
+  test("Test 1-Bit Demux - Selector: 0, Input: 0 must return Output1: 0, Output2: 0", () => {
+    expect(Component.demux1(0, 0)).toEqual({ output1: 0, output2: 0 });
+  })
+
+  test("Test 1-Bit Demux - Selector: 0, Input: 1 must return Output1: 1, Output2: 0", () => {
+    expect(Component.demux1(0, 1)).toEqual({ output1: 1, output2: 0 });
+  })
+
+  test("Test 1-Bit Demux - Selector: 1, Input: 0 must return Output1: 0, Output2: 0", () => {
+    expect(Component.demux1(1, 0)).toEqual({ output1: 0, output2: 0 });
+  })
+
+  test("Test 1-Bit Demux - Selector: 1, Input: 1 must return Output1: 0, Output2: 1", () => {
+    expect(Component.demux1(1, 1)).toEqual({ output1: 0, output2: 1 });
+  })
+});
