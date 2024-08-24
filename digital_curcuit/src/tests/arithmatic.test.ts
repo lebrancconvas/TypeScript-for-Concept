b8e905387bb7739184230634837a6ee3b10c255d
lebrancconvas <lebrancconvas@gmail.com>
2024-08-24 15:07:24 +0700

    ✅ test(digital_curcuit): update passed full adder arithmatic component test

--- digital_curcuit/src/tests/arithmatic.test.ts
+++ digital_curcuit/src/tests/arithmatic.test.ts
@@ -20,6 +20,40 @@ describe("Test Arithmatic: Half-Adder", () => {
   })
 });
 
+describe("Test Arithmatic: Full-Adder", () => {
+  test("Test Full-Adder - Input1: 0, Input2: 0, Input3: 0 must return Sum: 0, Carry: 0", () => {
+    expect(Arithmatic.fullAdder(0, 0, 0)).toEqual([0, 0]);
+  })
+
+  test("Test Full-Adder - Input1: 0, Input2: 0, Input3: 1 must return Sum: 1, Carry: 0", () => {
+    expect(Arithmatic.fullAdder(0, 0, 1)).toEqual([1, 0]);
+  })
+
+  test("Test Full-Adder - Input1: 0, Input2: 1, Input3: 0 must return Sum: 1, Carry: 0", () => {
+    expect(Arithmatic.fullAdder(0, 1, 0)).toEqual([1, 0]);
+  })
+
+  test("Test Full-Adder - Input1: 0, Input2: 1, Input3: 1 must return Sum: 0, Carry: 1", () => {
+    expect(Arithmatic.fullAdder(0, 1, 1)).toEqual([0, 1]);
+  })
+
+  test("Test Full-Adder - Input1: 1, Input2: 0, Input3: 0 must return Sum: 1, Carry: 0", () => {
+    expect(Arithmatic.fullAdder(1, 0, 0)).toEqual([1, 0]);
+  })
+
+  test("Test Full-Adder - Input1: 1, Input2: 0, Input3: 1 must return Sum: 0, Carry: 1", () => {
+    expect(Arithmatic.fullAdder(1, 0, 1)).toEqual([0, 1]);
+  })
+
+  test("Test Full-Adder - Input1: 1, Input2: 1, Input3: 0 must return Sum: 0, Carry: 1", () => {
+    expect(Arithmatic.fullAdder(1, 1, 0)).toEqual([0, 1]);
+  })
+
+  test("Test Full-Adder - Input1: 1, Input2: 1, Input3: 1 must return Sum: 1, Carry: 1", () => {
+    expect(Arithmatic.fullAdder(1, 1, 1)).toEqual([1, 1]);
+  })
+});
+
 describe("Test Arithmatic: 1-Bit Comparator (comparator1)", () => {
   test("Test Comparator1 - Input1: 0, Input2: 0 must return lessThan: 0, equal: 1, greaterThan: 0", () => {
     expect(Arithmatic.comparator1(0, 0)).toEqual([0, 1, 0]);
