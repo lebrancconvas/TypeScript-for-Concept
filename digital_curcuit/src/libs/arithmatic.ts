cd93366412296eae7b37410a94385dec6f1ca640
lebrancconvas <lebrancconvas@gmail.com>
2024-08-24 14:31:15 +0700

    ✨ feat(digital_curcuit): add comparator arithmatic component

--- digital_curcuit/src/libs/arithmatic.ts
+++ digital_curcuit/src/libs/arithmatic.ts
@@ -17,9 +17,9 @@ export const halfAdder = (input1: BitElement, input2: BitElement): [BitElement, 
 /**
  * Full-Adder Component is an arithmetic component that received three inputs and return sum and carry.
  * @name fullAdder
- * @param input1
- * @param input2
- * @param input3
+ * @param input1 The First Input Signal (1-Bit).
+ * @param input2 The Second Input Signal (1-Bit).
+ * @param input3 The Third Input Signal (1-Bit).
  * @returns sum, carry
  */
 export const fullAdder = (input1: BitElement, input2: BitElement, input3: BitElement): [BitElement, BitElement] => {
@@ -28,6 +28,19 @@ export const fullAdder = (input1: BitElement, input2: BitElement, input3: BitEle
   return [sum, carry];
 };
 
+/**
+ * 1-Bit Comparator Component is an arithmetic component that received two inputs and return less than, equal, and greater than.
+ * @name comparator1
+ * @param input1 The First Input Signal (1-Bit).
+ * @param input2 The Second Input Signal (1-Bit).
+ * @returns lessThan, equal, greaterThan
+ */
+export const comparator1 = (input1: BitElement, input2: BitElement): [BitElement, BitElement, BitElement] => {
+  let lessThan = Gate.and(Gate.not(input1), input2);
+  let equal = Gate.xor(input1, input2);
+  let greaterThan = Gate.and(input1, Gate.not(input2));
+  return [lessThan, equal, greaterThan];
+};
 
 export class Arithmetic {
   static halfAdder = halfAdder;
