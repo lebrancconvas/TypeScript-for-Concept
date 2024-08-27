4cfbdc9d32f4576d189680620a96857d809aaf8c
lebrancconvas <lebrancconvas@gmail.com>
2024-08-27 09:23:50 +0700

    💡 chore(vm): add comment for explaining each memory data

--- vm-16-bits/src/libs/episode_02/index.ts
+++ vm-16-bits/src/libs/episode_02/index.ts
@@ -16,24 +16,24 @@ export function ep02() {
   // Write data on memory.
   const writeableBytes = new Uint8Array(memory.buffer);
   const memoryData = [
-    Instruction.MOV_MEM_REG,
-    0x01,
-    0x00,
-    R1,
-
-    Instruction.MOV_LIT_REG,
+    Instruction.MOV_LIT_REG, // mov <lit> <reg>: Move / Assign Literal Value to Register.
     0x12,
-    0x34,
-    R2,
+    0x34, // 0x1234 -> Literal Value (16-Bits or 2 Bytes)
+    R1, // R1 Register
 
-    Instruction.ADD_REG_REG,
-    R1,
-    R2,
+    Instruction.MOV_LIT_REG, // mov <lit> <reg>: Move / Assign Literal Value to Register.
+    0xAB,
+    0xCD, // 0xABCD -> Literal Value (16-Bits or 2 Bytes)
+    R2, // R2 Register
 
-    Instruction.MOV_REG_MEM,
-    ACC,
+    Instruction.ADD_REG_REG, // add <reg>, <reg>: Arithmetic Addition of Register Values and Stored in ACC Register.
+    R1, // R1 Register
+    R2, // R2 Register
+
+    Instruction.MOV_REG_MEM, // mov <reg> <mem>: Move / Assign Register Value to Memory Address.
+    ACC, // ACC Register
     0x01,
-    0x00
+    0x00 // 0x0100 -> Memory Address (16-Bits or 2 Bytes)
   ];
 
   for(let i = 0; i < memoryData.length; i++) {
@@ -41,24 +41,28 @@ export function ep02() {
   }
 
   // Execute instructions.
+  cpu.viewMemory(cpu.getRegister("ip"));
   cpu.viewMemory(0x0100);
   cpu.debug();
   cpu.step();
 
+  cpu.viewMemory(cpu.getRegister("ip"));
   cpu.viewMemory(0x0100);
   cpu.debug();
   cpu.step();
 
+  cpu.viewMemory(cpu.getRegister("ip"));
   cpu.viewMemory(0x0100);
   cpu.debug();
   cpu.step();
 
+  cpu.viewMemory(cpu.getRegister("ip"));
   cpu.viewMemory(0x0100);
   cpu.debug();
   cpu.step();
 
+  cpu.viewMemory(cpu.getRegister("ip"));
   cpu.viewMemory(0x0100);
   cpu.debug();
-
 
 };
