026c6d2930f00373a1eb8a47b549013f16683b60
lebrancconvas <lebrancconvas@gmail.com>
2024-09-28 02:38:50 +0700

    ✨ feat(nes): update bus class

--- nes/src/components/bus.ts
+++ nes/src/components/bus.ts
@@ -1,3 +1,25 @@
-export class Bus {
+import { Memory } from "./memory";
+import type { MemoryLike } from "~/@types/components";
+import type { Uint8, Uint16 } from "~/@types/bits";
 
+export class Bus {
+  RAM: MemoryLike;
+
+  constructor() {
+    this.RAM = Memory.create(64 * (2 ** 10)); // RAM: 64 KiloBytes (64 KBs)
+  }
+
+  write(address: Uint16, data: Uint8) {
+    if(address >= 0x0000 && address <= 0xFFFF) {
+      this.RAM.setUint8(address, data);
+    }
+  }
+
+  read(address: Uint16, isReadOnly: boolean) {
+    if(address >= 0x0000 && address <= 0xFFFF) {
+      return this.RAM.getUint8(address);
+    }
+
+    return 0x00;
+  }
 };
